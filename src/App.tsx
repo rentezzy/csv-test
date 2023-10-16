ccbcfb1d5a446412974c119a9146092c0aaa5463
rentezzy <ren2ren4ik@gmail.com>
2023-10-16 21:34:25 +0300

    Add data display, styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
-import { parsedData } from "./utils/csvParser";
+import { headers, parsedData } from "./utils/csvParser";
 
 export function App() {
-  console.log(parsedData);
-  return <div></div>;
+  return (
+    <main className="container">
+      <table className="table">
+        <thead className="table__head">
+          <th>ID</th>
+          {headers.map((header) => (
+            <th>{header}</th>
+          ))}
+          <th>Duplicate with</th>
+        </thead>
+        <tbody className="table__body">
+          {parsedData.map((employee, index) => (
+            <tr key={employee.email + employee.phone}>
+              <td>{index}</td>
+              {Object.values(employee).map((value) => (
+                <td key={value}>{value}</td>
+              ))}
+              <td>-</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
+  );
 }
--- src/utils/csvParser.ts
+++ src/utils/csvParser.ts
@@ -1,3 +1,5 @@
+// Csv parser is needed to map types from human readable to JS standard, trim and parse some strings into numbers.
+
 import _data from "../data.csv";
 const data = _data as rawCsv[];
 type rawCsv = {
@@ -26,21 +28,21 @@ export type DataType = {
 };
 
 export const parsedData: DataType[] = [];
-export const headers = Object.keys(data);
+export const headers = Object.keys(data[0]);
 
 for (const dataItem of data) {
   const parsedDataItem: Partial<DataType> = {};
   parsedDataItem["fullName"] = dataItem["Full Name"].trim();
   parsedDataItem["phone"] = dataItem["Phone"].trim();
   parsedDataItem["email"] = dataItem["Email"].trim();
-  parsedDataItem["hasChildren"] = dataItem["Has children"].trim();
-  parsedDataItem["licenseStates"] = dataItem["License states"].trim();
-  parsedDataItem["expirationDate"] = dataItem["Expiration date"].trim();
-  parsedDataItem["licenseNumber"] = dataItem["License number"].trim();
   parsedDataItem["age"] = parseInt(dataItem["Age"]);
   parsedDataItem["experience"] = parseInt(dataItem["Experience"]);
   parsedDataItem["yearlyIncome"] = parseFloat(
     dataItem["Yearly Income"]
   ).toFixed(2);
+  parsedDataItem["hasChildren"] = dataItem["Has children"].trim();
+  parsedDataItem["licenseStates"] = dataItem["License states"].trim();
+  parsedDataItem["expirationDate"] = dataItem["Expiration date"].trim();
+  parsedDataItem["licenseNumber"] = dataItem["License number"].trim();
   parsedData.push(parsedDataItem as DataType);
 }
